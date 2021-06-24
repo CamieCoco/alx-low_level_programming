3cde88249802fc43cb6feebe46e826cdd4e7fe9d
Camella Enyi <enyicamella@gmail.com>
2021-06-24 13:11:31 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,52 @@
 #include "holberton.h"
-/**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
- */
 
+/**
+ * print_number - prints a number using _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_number(int n)
 {
-
-	int x = 1, i = 0, y = 0;
-
-	if (n < 0)
+	if (n >= 0)
 	{
-		_putchar('-');
+		num_to_char(n);
+	}
+	else
+	{
 		n = -n;
+		_putchar('-');
+		num_to_char(n);
 	}
+}
 
-	while (n / x != 0)
+/**
+ * num_to_char - transforms a number with 1 or more digits into a char
+ * @n: number to print
+ *
+ * Return: void
+ */
+void num_to_char(int n)
+{
+	int d = 10;
+
+	if (n < d)
 	{
-		x *= 10;
-		i++;
-	}
-	x = x / 10;
-
-	while (y < i)
-	{
-		_putchar('0' + n / x);
-		n = n - (n / x) * x;
-		x = x / 10;
-		y++;
-	}
-
-	if (i == 0)
 		_putchar('0' + n);
-
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+		}
+		d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
