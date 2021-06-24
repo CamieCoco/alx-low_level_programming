880663339b8377f45ac51390c42f7809341b49b3
Camella Enyi <enyicamella@gmail.com>
2021-06-24 12:51:34 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,38 +1,29 @@
 #include "holberton.h"
-
 /**
- * print_diagonal - print diagonal signal
- * @n: value to create matriz
- * Return: void
+ * print_diagonal - function that draws a diagonal line on the terminal.
+ * @n: the int for the paramaters of my function
+ * Return: 0
  */
-
 void print_diagonal(int n)
 {
-	int i, j;
+	int a = 0;
+	int b = 0;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		while (a < n)
 		{
-			for (j = 0; j < n; j++)
+			while (b < a)
 			{
-				if (i > j)
-<<<<<<< HEAD
-<<<<<<< HEAD
-					putchar('\\');
-			} 
-				_putchar('\n');
-=======
-=======
->>>>>>> 392d491848f636c4e835156c05c1cfd685354ece
-					_putchar(32);
-				if (i == j)
-					_putchar('\\');
+				_putchar(' ');
+				b++;
 			}
+			a++;
+			b = 0;
+			_putchar('\\');
 			_putchar('\n');
->>>>>>> 392d491848f636c4e835156c05c1cfd685354ece
 		}
 	}
 	else
-		_putchar('\n');
+	_putchar('\n');
 }
