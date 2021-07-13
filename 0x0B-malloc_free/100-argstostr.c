b4b945c012d316ddedade38650961f09b986e689
Camella Enyi <enyicamella@gmail.com>
2021-07-13 10:38:18 -0700

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,44 +1,44 @@
 #include "holberton.h"
-#include <stdlib.h>
-#include <stdio.h>
-/**
- * argstostr - concatenate all arguments of your program with newline
- * @ac: argument count
- * @av: double pointer to array of strings passed to main
- * Return: Null if fail, else return pointer to new string
- */
 
+/**
+ * argstostr - that concatenates all the arguments of your program
+ * @ac: integer
+ * @av: doble pointer character
+ *
+ * Return: a pointer to a new string
+ */
 char *argstostr(int ac, char **av)
 {
-	char *a, *retp;
-	int i, j, total;
+	int i, j, longitud;
+	char *ptr;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0, total = 0; i < ac; i++)
 	{
-		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
-			;
-		total++;
-	}
-	total++;
-
-	a = malloc(total * sizeof(char));
-	if (a == NULL)
 		return (NULL);
-
-	retp = a;
+	}
+	longitud = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			*a = av[i][j];
-			a++;
+			longitud++;
 		}
-		*a = '\n';
-		a++;
 	}
-
-	return (retp);
+	ptr = malloc((longitud + ac + 1) * sizeof(char));
+	longitud = 0;
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[longitud] = av[i][j];
+			longitud++;
+		}
+		ptr[longitud] = '\n';
+		longitud++;
+	}
+	return (ptr);
 }
