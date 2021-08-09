a7e42ac403855e08fe4247a4e55bafc0ef457776
Camella Enyi <enyicamella@gmail.com>
2021-08-09 12:48:25 -0700

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,14 +2,18 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-#endif
+void cp_file(char *file1, char *file2, size_t bytes);
+void printf_error_read(char *filename);
+void printf_error_write(char *filename);
+void printf_error_close(int filename);
+
+#endif /* HOLBERTON_H */
