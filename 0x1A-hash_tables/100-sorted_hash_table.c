a8ec425810b8d0a08fbdf7fdd8d887eea292f495
Camella Enyi <enyicamella@gmail.com>
2021-10-08 23:00:20 +0000

    hash table

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - creates a shash table with a given size
+ *
+ * @size: size of the shash table
+ * Return: the created shash table, or NULL if function fails
+ */
+shash_table_t *shash_table_create(unsigned long int size)
