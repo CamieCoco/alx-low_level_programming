ec5eb2bcd6a13f13a0d73a38e5d19a795944ec07
CamieCoco <75216613+CamieCoco@users.noreply.github.com>
2021-10-08 23:53:46 +0100

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,34 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - function that retrieves a value associated with a key.
- * @ht:  is the hash table you want to look into
- * @key: is the key you are looking for
+ * hash_table_get - retrieves a value associated with a key
  *
- * Return:  the value associated with the element,
- * or NULL if key t be found
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * Return: value of the hash.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int i;
-  char *value_list;
-  hash_node_t *tmp;
+	unsigned long int k_index;
+	hash_node_t *tmp;
 
-  if (ht == NULL)
-    return (NULL);
+	if (ht == NULL)
+		return (NULL);
 
-  value_list = strdup(key);
-  i = key_index((unsigned char *)key, ht->size);
-  tmp = ht->array[i];
-  while (tmp != NULL)
-    {
-      if (strcmp(tmp->key, value_list) == 0)
-	break;
-      tmp = tmp->next;
-    }
-  free(value_list);
-  if (tmp == NULL)
-    return (NULL);
+	if (key == NULL || *key == '\0')
+		return (NULL);
 
-  return (tmp->value);
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[k_index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
 }
